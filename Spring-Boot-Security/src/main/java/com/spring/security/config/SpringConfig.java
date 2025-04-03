0455bdb2ea5d9f2b78b902ad011b487c3f519064
sonawanesurajvitthal <surajsonawane8483@gmail.com>
2025-04-03 18:48:18 +0530

    Jwt implementation

--- Spring-Boot-Security/src/main/java/com/spring/security/config/SpringConfig.java
+++ Spring-Boot-Security/src/main/java/com/spring/security/config/SpringConfig.java
@@ -2,6 +2,10 @@ package com.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
@@ -13,11 +17,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JwtFilter jwtFilter;
+
+    public SpringConfig(UserDetailsService userDetailsService, JwtFilter jwtFilter){
+        this.userDetailsService = userDetailsService;
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -32,14 +45,23 @@ public class SpringConfig {
                        .requestMatchers("/demo").hasAuthority("write")
                        .anyRequest().authenticated())
                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                .formLogin(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
 }
--- Spring-Boot-Security/src/main/java/com/spring/security/service/CustomUserService.java
+++ Spring-Boot-Security/src/main/java/com/spring/security/service/CustomUserService.java
@@ -4,9 +4,13 @@ import com.spring.security.entity.User;
 import com.spring.security.exception.UserAlreadyExistException;
 import com.spring.security.module.SecurityUser;
 import com.spring.security.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +20,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class CustomUserService implements UserDetailsService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-    public CustomUserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public CustomUserService(UserRepository userRepository, JWTService jwtService, AuthenticationManager authenticationManager){
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     @Override
@@ -38,8 +47,17 @@ public class CustomUserService implements UserDetailsService {
         if(u != null){
             throw new UserAlreadyExistException();
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(encoder.encode(user.getPassword()));
         userRepository.save(user);
 
+    }
+
+    public String verify(User user) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(user.getUsername());
+        } else {
+            return "fail";
+        }
     }
 }
